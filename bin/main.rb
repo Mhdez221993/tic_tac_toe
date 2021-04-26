6e97ee19e0b142a332881a1bb035ab0ede1aba20
Moises <mhdezcoronado@gmail.com>
2021-04-26 14:51:46 +0200

    Add while on game

--- bin/main.rb
+++ bin/main.rb
@@ -7,47 +7,64 @@ def check_input(cell)
   end
 end
 
+$block = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+$maped = {
+  1 => [0,0],
+  2 => [0,1],
+  3 => [0,2],
+  4 => [1,0],
+  5 => [1,1],
+  6 => [1,2],
+  7 => [2,0],
+  8 => [2,1],
+  9 => [2,2]
+}
+
 def show_board
   system "clear" or system "cls"
-  $block = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-
-  puts "x---x---x---x"
-  puts "| #{block[0][0]} | #{block[0][1]} | #{block[0][2]} |"
-  puts "x---x---x---x"
-  puts "| #{block[1][0]} | #{block[1][1]} | #{block[1][2]} |"
-  puts "x---x---x---x"
-  puts "| #{block[2][0]} | #{block[2][1]} | #{block[2][2]} |"
-  puts "x---x---x---x"
 end
 
-puts "Welcome to Tic-Tac-Toe!"
-puts "\n"
-puts "What's the name of the first player?"
-
-player1 = gets.chomp
-
-puts "\n"
-puts "What's the name of the second player?"
-
-player2 = gets.chomp
-
-puts "\n"
-
-show_board
-
-puts "\n"
-puts "#{player1} will play X and #{player2} will play O"
-puts "\n"
-puts "Please #{player1} select an available cell from the board:"
-
-cell1 = gets.chomp.to_i
-
-check_input(cell1)
-
-puts "Please #{player2} select an available cell from the board:"
-
-cell2 = gets.chomp.to_i
-
-check_input(cell2)
-
-show_board
+while true
+  p $block
+  puts "x---x---x---x"
+  puts "| #{$block[0][0]} | #{$block[0][1]} | #{$block[0][2]} |"
+  puts "x---x---x---x"
+  puts "| #{$block[1][0]} | #{$block[1][1]} | #{$block[1][2]} |"
+  puts "x---x---x---x"
+  puts "| #{$block[2][0]} | #{$block[2][1]} | #{$block[2][2]} |"
+  puts "x---x---x---x"
+  
+  # puts "Welcome to Tic-Tac-Toe!"
+  # puts "\n"
+  # puts "What's the name of the first player?"
+  
+  # player1 = gets.chomp
+  
+  # puts "\n"
+  # puts "What's the name of the second player?"
+  
+  # player2 = gets.chomp
+  
+  # puts "\n"
+  
+  # # show_board
+  
+  # puts "\n"
+  # puts "#{player1} will play X and #{player2} will play O"
+  # puts "\n"
+  # puts "Please #{player1} select an available cell from the board:"
+  
+  x = gets.chomp.to_i
+  check_input(x)
+  first = $maped[x]
+  $block[first[0]][first[1]] = 'X' unless $block[first[0]][first[1]] == 'O' || $block[first[0]][first[1]] == 'X'
+  
+  # puts "Please #{player2} select an available cell from the board:"
+  
+  o = gets.chomp.to_i
+  check_input(o)
+  second = $maped[o]
+  $block[second[0]][second[1]] = 'O' unless $block[second[0]][second[1]] == 'X' || $block[second[0]][second[1]] == 'O'
+  
+  show_board
+end
