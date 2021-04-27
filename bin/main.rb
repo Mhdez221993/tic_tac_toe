19adfff71aa102c898aae51d2f5b19d97ebe2324
Moises <mhdezcoronado@gmail.com>
2021-04-27 10:41:38 +0200

    Add user interface

--- bin/main.rb
+++ bin/main.rb
@@ -9,8 +9,8 @@ end
 
 def show_board
   system "clear" or system "cls"
-  $block = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-
+  
+  block = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
   puts "x---x---x---x"
   puts "| #{block[0][0]} | #{block[0][1]} | #{block[0][2]} |"
   puts "x---x---x---x"
@@ -18,6 +18,7 @@ def show_board
   puts "x---x---x---x"
   puts "| #{block[2][0]} | #{block[2][1]} | #{block[2][2]} |"
   puts "x---x---x---x"
+  puts "\n"
 end
 
 puts "Welcome to Tic-Tac-Toe!"
@@ -38,16 +39,28 @@ show_board
 puts "\n"
 puts "#{player1} will play X and #{player2} will play O"
 puts "\n"
-puts "Please #{player1} select an available cell from the board:"
 
-cell1 = gets.chomp.to_i
+counter = 9
+while counter > 0
+  puts "remaining moves #{counter}."
+  puts "\n"
+  puts "Please #{player1} select an available cell from the board:"
+  cell1 = gets.chomp.to_i
+  check_input(cell1)
+  counter -= 1
+  if counter == 0
+    puts "\n"
+    puts "is a tie!"
+    exit
+  end
 
-check_input(cell1)
+  puts "remaining moves #{counter}."
+  puts "\n"
+  puts "Please #{player2} select an available cell from the board:"
+  cell2 = gets.chomp.to_i
+  check_input(cell2)
+  show_board
+  counter -= 1
+  
+end
 
-puts "Please #{player2} select an available cell from the board:"
-
-cell2 = gets.chomp.to_i
-
-check_input(cell2)
-
-show_board
