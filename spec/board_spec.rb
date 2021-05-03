9d0612e44785d37684d85016439b19258b7c13f2
Moises <mhdezcoronado@gmail.com>
2021-05-03 10:51:35 +0200

    Update it messages

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,7 +3,7 @@ require_relative '../lib/board.rb'
 module TicTacToe
     describe Board do
       describe ".initialize" do
-        it "return un empty hash" do
+        it "return three nested arrys" do
             arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
             board = Board.new
             expect(board.grid).to eq arr
@@ -11,7 +11,7 @@ module TicTacToe
       end
 
       describe "#default_grid" do
-        it "return un empty hash" do
+        it "return three nested arrys" do
             arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
             board = Board.new
             expect(board.default_grid).to eq arr
@@ -19,15 +19,14 @@ module TicTacToe
       end
 
       describe "#get_cell" do
-        it "return un empty hash" do
-            arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+        it "return number nine" do
             board = Board.new
             expect(board.get_cell(2,2)).to eq 9
         end
       end
 
       describe "#set_cell" do
-        it "return un empty hash" do
+        it "return and X" do
             board = Board.new
             expect(board.set_cell(2,2,"X")).to eq "X"
         end
