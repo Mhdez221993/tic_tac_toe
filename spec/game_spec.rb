7c66e916c865d976135dc7da8c1f5d0a5883eb76
Safa <safaaballagh97@gmail.com>
2021-05-03 12:23:13 +0000

    Add test for posible_winning

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -27,6 +27,14 @@ module TicTacToe
             expect(game.check_if_draw).to eq true
         end
       end
+
+      
+      describe "#posible_winning" do
+        it "return array with all the possibilities" do
+          wins = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+            expect(game.posible_winning).to eq wins
+        end
+      end
     end
 end
 
