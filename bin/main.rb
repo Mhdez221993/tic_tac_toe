be79b47d9996e4755d68ec8a2e13ce68832ff32b
Moises Hernandez <mhdezcoronado@gmail.com>
2025-01-24 18:07:48 +0000

    Refactor player name verification and improve user prompts in main.rb

--- bin/main.rb
+++ bin/main.rb
@@ -4,24 +4,25 @@ require_relative '../lib/board'
 require_relative '../lib/player'
 
 def verify_name(name)
-  player = name
-  while player.empty?
-    puts 'pleate type a valid name'
-    player = gets.chomp
+  while name.strip.empty?
+    puts 'Please type a valid name:'
+    name = gets.chomp
   end
-  player
+  name
+end
+
+def clear_screen
+  system 'clear' or system 'cls'
 end
 
 puts 'Welcome to Tic-Tac-Toe!'
 puts "\n"
 
 puts "What's the name of the first player?"
-
 player1 = verify_name(gets.chomp)
 
 puts "\n"
 puts "What's the name of the second player?"
-
 player2 = verify_name(gets.chomp)
 
 puts "\n"
@@ -33,7 +34,7 @@ module TicTacToe
   class Game
     def show_board(game)
       b = game.board
-      system 'clear' or system 'cls'
+      clear_screen
       puts 'x---x---x---x'
       puts "| #{b.get_cell(0, 0)} | #{b.get_cell(0, 1)} | #{b.get_cell(0, 2)} |"
       puts 'x---x---x---x'
@@ -45,34 +46,34 @@ module TicTacToe
     end
 
     def call_player(player)
-      puts "It's #{player} turn:"
+      puts "It's #{player}'s turn. Enter a number between 1 and 9:"
       number = gets.chomp.to_i
       until number.between?(1, 9)
-        puts 'Please enter a number between 1 and 9'
+        puts 'Invalid input. Please enter a number between 1 and 9:'
         number = gets.chomp.to_i
       end
       number.to_s
     end
 
     def call_draw
-      puts "\n"
-      puts 'It\'s a tie!'
-      puts "\n"
+      puts "\nIt's a tie!\n\n"
       play_again?
     end
 
     def play_again?
-      puts 'Do you wanna try again? [Y/N]'
-      ans = gets.chomp.to_s.upcase
+      puts 'Do you want to play again? [Y/N]'
+      ans = gets.chomp.upcase
       case ans
       when 'Y'
-        system 'clear' or system 'cls'
+        clear_screen
         system 'bin/main.rb'
       when 'N'
-        system 'clear' or system 'cls'
+        clear_screen
         exit
+      else
+        puts 'Invalid input. Please enter Y or N.'
+        play_again?
       end
-      play_again?
     end
 
     def announce_winner(winner)
@@ -80,8 +81,7 @@ module TicTacToe
     end
   end
 end
-puts "\n"
-puts "#{player1} will play X and #{player2} will play O"
-puts "\n"
+
+puts "\n#{player1} will play X and #{player2} will play O\n\n"
 
 game.play
